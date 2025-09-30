c30cbe9954956edd1ca8bb095f14500251177ed2
Michael <michael.elliston95@gmail.com>
2025-09-30 10:05:09 -0500

    final commit, seems to work and print the proper format!

--- src/main/java/com/pluralsight/AddressBuilder.java
+++ src/main/java/com/pluralsight/AddressBuilder.java
@@ -1,4 +1,51 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class AddressBuilder {
+    static Scanner myScanner = new Scanner(System.in);
+    static StringBuilder addressBuilder = new StringBuilder();
+    public static void main(String[] args) {
+
+        System.out.println("Please input your Full Name: ");
+        inputScanner();
+        addressBuilder.append("\n\nBilling Address: \n");
+
+        System.out.println("Please input your Billing Street: ");
+        inputScanner();
+        addressBuilder.append("\n");
+
+        System.out.println("Please input your Billing City: ");
+        inputScanner();
+        addressBuilder.append(", ");
+
+        System.out.println("Please input your Billing State: ");
+        inputScanner();
+        addressBuilder.append(" ");
+
+        System.out.println("Please input your Billing Zip Code: ");
+        inputScanner();
+        addressBuilder.append("\n\nShipping Address: \n");
+
+        System.out.println("Please input your Shipping Street: ");
+        inputScanner();
+        addressBuilder.append("\n");
+
+        System.out.println("Please input your Shipping City: ");
+        inputScanner();
+        addressBuilder.append(", ");
+
+        System.out.println("Please input your Shipping State: ");
+        inputScanner();
+        addressBuilder.append(" ");
+
+        System.out.println("Please input your Shipping Zip Code: ");
+        inputScanner();
+
+        System.out.println(addressBuilder);
+
+    }
+    public static void inputScanner() {
+        addressBuilder.append(myScanner.nextLine());
+    }
 }
